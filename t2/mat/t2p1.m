05c95f2b62711cfe0114547774940f39896391d3
TiagoSilvaIST <tiago.silva.2001@tecnico.ulisboa.pt>
2021-04-06 00:34:41 +0100

    graphics and tablees to tex

--- t2/mat/t2p1.m
+++ t2/mat/t2p1.m
@@ -14,8 +14,7 @@ C = 1.03087754949e-6
 Kb = 7.2492497599e-3 
 Kd = 8.29575903566e3
 
-
-
+%%%%%%%%%%THEO - P1%%%%%%%%%%
 
 %%In this part, all of the node voltages will be calculated using the Nodal Analysis Method
 
@@ -43,12 +42,9 @@ T=[Vs;0;0;0;0;0;0;0;0;0]
 
 s=R\T
 
-
-
-
 %%This will print the results of the Nodal Analysis in a table
 
-file1=fopen('octave_tab_voltage.tex', 'w');
+file1=fopen('theo_first.tex', 'w');
 
 fprintf(file1, '\n Node Voltage 1 & %.11e \\\\ \\hline ', s(1));
 fprintf(file1, '\n Node Voltage 2 & %.11e \\\\ \\hline ', s(2));
@@ -58,9 +54,7 @@ fprintf(file1, '\n Node Voltage 6 & %.11e \\\\ \\hline ', s(5));
 fprintf(file1, '\n Node Voltage 7 & %.11e \\\\ \\hline ', s(6));
 fprintf(file1, '\n Node Voltage 8 & %.11e \\\\ \\hline ', s(7));
 
-
 fclose(file1);
-
 
 vaux=s(5)-s(7) 
 
@@ -70,10 +64,7 @@ fprintf(file2, 'Vx n6 n8 %.11e\n', vaux);
 
 fclose(file2);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
+%%%%%%%%%%THEO - P2%%%%%%%%%%
 
 Vs=0
 Vx=s(5)-s(7)
@@ -95,6 +86,7 @@ X=[1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0, 0;
 0 , 1/r3 , 0 , (-1/r3-1/r4-1/r5) , 1/r5 , 0, 0 , 0 , 1 , 1;
 
 0 , 0 , 0 , 0 , 0 , 1/r6 , 0 , 0, 0, 1;
+
 0 , 0 , 0 , 0 , 1 , 0    , -1 , 0, 0, 0 ]
 
 Y=[Vs;0;0;0;0;0;0;0;0;Vx]
@@ -113,7 +105,23 @@ printf("A Constante do Tempo equivalente é %f \n", tau)
 
 
 
+
+file3=fopen('theo_second.tex', 'w');
+
+fprintf(file3, '\n Node Voltage 1 & %.11e \\\\ \\hline ', w(1));
+fprintf(file3, '\n Node Voltage 2 & %.11e \\\\ \\hline ', w(2));
+fprintf(file3, '\n Node Voltage 3 & %.11e \\\\ \\hline ', w(3));
+fprintf(file3, '\n Node Voltage 5 & %.11e \\\\ \\hline ', w(4));
+fprintf(file3, '\n Node Voltage 6 & %.11e \\\\ \\hline ', w(5));
+fprintf(file3, '\n Node Voltage 7 & %.11e \\\\ \\hline ', w(6));
+fprintf(file3, '\n Node Voltage 8 & %.11e \\\\ \\hline ', w(7));
+fprintf(file3, '\n Req, Equivalent Resistor & %f \\\\ \\hline ', req);
+fprintf(file3, '\n Time Constant & %f \\\\ \\hline ', tau);
+
+fclose(file3);
+
 %%%%%%%%%%%%%%%%%%%%%%NATURAL%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%THEO - P3%%%%%%%%%%
 
 t=0:1e-6:20e-3;
 
@@ -129,14 +137,15 @@ print (hn, "natural.eps", "-depsc");
 
 
 
-file3=fopen('datafrom2.txt', 'w');
+file4=fopen('datafrom2.txt', 'w');
 
-fprintf(file3, '.ic v(n6)=%.11e v(n8)=%.11e\n', w(5), w(7));
+fprintf(file4, '.ic v(n6)=%.11e v(n8)=%.11e\n', w(5), w(7));
 
-fclose(file3);
+fclose(file4);
 
 
 %%%%%%%%%%%%%%%%%%%%%%FORCED%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%THEO - P4%%%%%%%%%%
 
 f = 1000 %Hz
 w = 2*pi*f; %rad/s
@@ -178,9 +187,22 @@ xlabel ("t[ms]");
 ylabel ("v6f(t) [V]");
 print (hf, "forced.eps", "-depsc");
 
+file5=fopen('theo_fourth.tex', 'w');
+
+fprintf(file5, '\n Node Voltage 1 & %.11e \\\\ \\hline ', l(1));
+fprintf(file5, '\n Node Voltage 2 & %.11e \\\\ \\hline ', l(2));
+fprintf(file5, '\n Node Voltage 3 & %.11e \\\\ \\hline ', l(3));
+fprintf(file5, '\n Node Voltage 5 & %.11e \\\\ \\hline ', l(4));
+fprintf(file5, '\n Node Voltage 6 & %.11e \\\\ \\hline ', l(5));
+fprintf(file5, '\n Node Voltage 7 & %.11e \\\\ \\hline ', l(6));
+fprintf(file5, '\n Node Voltage 8 & %.11e \\\\ \\hline ', l(7));
+
+fclose(file5);
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%TOTAL
-
+%%%%%%%%%%THEO - P5%%%%%%%%%%
 
 v6t=v6n+v6f;
 ht=figure ();
